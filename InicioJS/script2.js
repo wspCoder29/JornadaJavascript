bb01fa85cefbfdc7938f307f7ad13553ff6887f4
Leandro Ferreira <86746732+wspCoder29@users.noreply.github.com>
2022-05-10 19:45:14 -0300

    Add files via upload

---
+++ InicioJS/script2.js
@@ -0,0 +1,29 @@
+//Template literals
+
+let firstName = 'Leandro';
+let lastName = 'Ferreira';
+let birthYear = 1983;
+
+console.log("Usando Template literals");
+
+let frase = 'Eu sou ' + firstName + ' ' + lastName + ' nasci em ' + birthYear;
+
+console.log(frase);
+
+let novaFrase = `Eu sou ${firstName} ${lastName} nasci em ${birthYear}`;
+
+console.log(novaFrase);
+
+//Usando backticks
+
+console.log(`Teste teste teste`);
+
+console.log('String with \n\
+multiple \n\
+lines ');
+
+
+console.log(`string 
+with 
+multiple
+lines`);
